f81c5ea3b7f15dc40bb227f6061d2897ee2c149c
carlosleonardo <carlos_leonardo.goes_farias@yahoo.com.br>
2013-05-07 15:44:52 -0300

    Alteração para usar credenciais para autenticar usuário.

--- compromissos/src/test/java/br/gov/serpro/tutordemoiselle/compromissos/CompromissoTest.java
+++ compromissos/src/test/java/br/gov/serpro/tutordemoiselle/compromissos/CompromissoTest.java
@@ -1,19 +1,26 @@
 package br.gov.serpro.tutordemoiselle.compromissos;
 
 import javax.inject.Inject;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
 import br.gov.serpro.tutordemoiselle.compromissos.entidades.Compromisso;
 import br.gov.serpro.tutordemoiselle.compromissos.negocios.CompromissoBC;
+import br.gov.serpro.tutordemoiselle.compromissos.seguranca.Credenciais;
 
 @RunWith(DemoiselleRunner.class)
 public class CompromissoTest {
 	
 	@Inject
 	private CompromissoBC compromissoBC;
+	
+	@Inject
+	private SecurityContext contextoSeguranca;
+	
+	@Inject
+	private Credenciais credenciais;
 	
 	@Test
 	public void incluirCompromisso()
@@ -37,5 +44,14 @@ public class CompromissoTest {
 		Compromisso compromisso = new Compromisso("Dayhorc");
 		compromissoBC.insert(compromisso);
 		compromissoBC.delete(compromisso.getId());
+	}
+	
+	@Before
+	public void iniciar()
+	{
+		// Fazemos login e testamos
+		credenciais.setNome("carlos");
+		credenciais.setSenha("lao");
+		contextoSeguranca.login();
 	}
 }
